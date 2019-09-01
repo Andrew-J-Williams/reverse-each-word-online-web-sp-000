ddb10fa91817f0fa3f5b83b5b7cdbb8372b56cd0
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 14:24:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,11 @@ def reverse_each_word(string)
   our_word = []
   our_word << string
 
+  reversed_words = []
+
   our_word.each do |word|
-    word.reversed
-    
+    reversed_words << word.reversed
+
 
 
 end
