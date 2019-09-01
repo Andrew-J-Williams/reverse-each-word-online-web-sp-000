3560e3d1df620a63c5e5e1b1e90321463a7dc13a
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 14:29:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(string)
   reversed_words = []
 
   our_word.each do |word|
-    reversed_words.unshift(word.reverse)
+    reversed_words << word.reverse
   end
 
   reversed_words.join(" ")
