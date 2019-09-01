c85063e22c3654f19c6108b09f66ee528a5fb621
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 14:39:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-  new_array = string.split(" ")
+#def reverse_each_word(string) First, create a method that accepts the argument of a string.
+  #new_array = string.split(" ") Second, take that string and convert it into an array where each word in the string is an element.
 
-  reversed_array = []
+  #reversed_array = [] Third, create a second array to hold our reversed words.
 
-  new_array.each do |word|
-      reversed_array << word.reverse
-  end
+  #new_array.each do |word| Fourth, use the each method 
+  #    reversed_array << word.reverse
+  #end
 
-  reversed_array.join(" ")
+  #reversed_array.join(" ")
 
-end
+#end
